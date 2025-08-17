82ee390bd5b182656e9d6da8b0291b5a9d5b920f
fqidz <faidz.arante@gmail.com>
2025-08-17 08:42:29 +0300

    feat: update rust template

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,21 +1,30 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }:
-  let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      # config.allowUnfree = true;
-    };
-  in
-  {
-    devShells."x86_64-linux".default = pkgs.mkShell {
-      packages = [
-        pkgs.rustc
-        pkgs.rust-analyzer
-        pkgs.rustfmt
-        pkgs.cargo
-      ];
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
     };
   };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.cargo
+            pkgs.rust-analyzer
+            pkgs.rustc
+            pkgs.rustfmt
+            # pkgs.lld
+          ];
+        };
+      }
+    );
 }
